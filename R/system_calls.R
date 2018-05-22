1b65662f615b0f633fd92a5dbbc05d33c47b6a6a
Ildiko Czeller <czeildi@gmail.com>
2018-05-21 20:52:02 -0700

    use better argument name

--- R/system_calls.R
+++ R/system_calls.R
@@ -1,9 +1,9 @@
-find_system_calls <- function(my_expression) {
+find_system_calls <- function(expr) {
   system_function_names <- c("system2", "system", "run")
 
   withr::with_options(
     c("keep.source" = TRUE),
-    utils::getParseData(my_expression)
+    utils::getParseData(expr)
   ) %>%
     subset(.$token == "SYMBOL_FUNCTION_CALL") %>%
     subset(.$text %in% system_function_names)
