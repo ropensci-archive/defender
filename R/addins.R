bcbe0e2c321b9e75b74a9177c84bc2e3ec68bfdf
Ildiko Czeller <czeildi@gmail.com>
2018-06-02 23:23:00 +0200

    add rstudio addin for namespace check for system related imports

--- R/addins.R
+++ R/addins.R
@@ -21,3 +21,27 @@ system_calls_addin <- function() {
   viewer <- shiny::dialogViewer('system calls')
   shiny::runGadget(ui, server, viewer = viewer)
 }
+
+check_namespace_addin <- function() {
+
+  ui <- miniUI::miniPage(
+    miniUI::gadgetTitleBar("List system imports"),
+    miniUI::miniContentPanel(
+      shiny::textInput('evil_path', 'path:', value = '.')
+    )
+  )
+
+  server <- function(input, output, session) {
+
+    shiny::observeEvent(input$done, {
+      rstudioapi::sendToConsole(
+        paste0("defender::check_namespace('", input$evil_path, "')")
+      )
+      shiny::stopApp()
+    })
+
+  }
+
+  viewer <- shiny::dialogViewer('list system imports')
+  shiny::runGadget(ui, server, viewer = viewer)
+}
